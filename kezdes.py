77fbc27c4445c4ee61040766fba21c001942a6bd
CurtisMajka <11C-varga@ipari.vein.hu>
2025-02-07 12:56:47 +0100

    adatbázis pythonhoz csatolása (áron)

---
+++ kezdes.py
@@ -0,0 +1,6 @@
+import mysql.connector
+import mysql
+
+asd = mysql.connector.connect(host="localhost", user="root", password="", database="mozi_adatbazis")
+
+# python.exe -m pip install --upgrade pip
