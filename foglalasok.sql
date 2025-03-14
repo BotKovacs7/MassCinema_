e46574b36bb7db501c87f2690918c739cdd6b069
CurtisMajka <11c-varga@ipari.vein.hu>
2025-03-14 13:24:12 +0100

    orai (aron)

--- foglalasok.sql
+++ foglalasok.sql
@@ -23,6 +23,8 @@ INSERT INTO Termek (Terem_szam, Film_cime, Ev, Mufaj, Jatekido, Kapacitas) VALUE
 (3, 'Harry Potter', 2001, 'Fantasy', '152 min', 180),
 (4, 'Tenet', 2020, 'Akció', '150 min', 100),
 (5, 'The Matrix', 1999, 'Sci-Fi', '136 min', 200);
+(6, 'Inception', 2010, 'Akció', '148 min', 130),
+(7, 'The Dark Knight', 2008, 'Akció', '152 min', 180);
 
 INSERT INTO Foglalasok (Foglalas_sorszam, Keresztnev, Vezeteknev, Teremszam, Szekszam) VALUES
 (1, 'Anna', 'Kovács', 1, 23),
@@ -30,3 +32,5 @@ INSERT INTO Foglalasok (Foglalas_sorszam, Keresztnev, Vezeteknev, Teremszam, Sze
 (3, 'Csaba', 'Tóth', 2, 10),
 (4, 'Dóra', 'Kiss', 4, 30),
 (5, 'László', 'Horváth', 5, 15);
+(6, 'Zoltán', 'Nagy', 6, 25),
+(7, 'Eszter', 'Molnár', 7, 40);
